33de8f463780d6a010855f8732299ddd9e3851e2
Ian Storz <ian@gocirro.com>
2012-10-25 10:26:42 -0600

    core update and testing

--- sites/all/modules/cirro_ian/js/cirro_ian.js
+++ sites/all/modules/cirro_ian/js/cirro_ian.js
@@ -1,4 +1,5 @@
 Cirro.ajax({
+	$errorPlacement: jQuery( "div#content" ),
 	data: {
 		callback: "valid_ajax_callback_cjax",
 		test: "testing valid callback",
@@ -11,6 +12,7 @@ Cirro.ajax({
 });
 
 Cirro.ajax({
+	$errorPlacement: jQuery( "div#content" ),
 	data: {
 		callback: "invalid_ajax_callback",
 		test: "testing invalid callback",
