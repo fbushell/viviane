210d16a752c32f8b6fee6b4a3885f8bf90be00e0
fbushell <frank@gocirro.com>
2013-04-14 18:17:02 -0600

    basic randomizer working

--- sites/all/modules/viviane_list/js/list.js
+++ sites/all/modules/viviane_list/js/list.js
@@ -23,38 +23,55 @@
 			self.$nav.on("click", self.scrollTo);
 
 			self.listHover();
-
-	},
-
-	listHover: function(){
-		var self = viviane;
-		var $item = $('.list-item');
-
-		$item.on('hover', function(){
-			$(this).find('.list-img').toggleClass('hidden');
-		});
-	},
-
-	//
-	// Ohhhhhh baby that is some smooth scrolling
-	//
-	scrollTo: function(event){
-
-		var self = viviane;
-
-		// Init
-		var $link = $(this);
-
-		// Scroll to href of clicked link
-		$("html, body").stop().animate({
-
-      scrollTop: $($link.attr("href")).offset().top + 30
-
-    }, 1500,'easeInOutExpo');
-
-		event.preventDefault();
+			self.randomPosition();
 
 		},
+
+		listHover: function(){
+			var self = viviane;
+			var $item = $('.list-item');
+
+			$item.on('hover', function(){
+				var $this = $(this).find('.list-img');
+				$this.toggleClass('hidden');
+				self.randomPosition($this);
+			});
+		},
+
+		//
+		// Ohhhhhh baby that is some smooth scrolling
+		//
+		scrollTo: function(event){
+
+			var self = viviane;
+
+			// Init
+			var $link = $(this);
+
+			// Scroll to href of clicked link
+			$("html, body").stop().animate({
+
+	      scrollTop: $($link.attr("href")).offset().top + 30
+
+	    }, 1500,'easeInOutExpo');
+
+			event.preventDefault();
+
+			},
+
+			randomPosition: function(item){
+
+				var self = viviane;
+				var $item = $(item);
+
+				var winwidth	= $window.width();
+				var posx		= Math.floor(Math.random() * (winwidth - 800));
+				var posy		= Math.floor(Math.random() * 250) + -250;
+
+				console.log(winwidth);
+
+				$item.css({top:posy, right:posx});
+			}
 
 
 	}
