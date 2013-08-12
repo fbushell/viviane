cb202627f2f8c12277ccc42a5aa870bcdc68c999
fbushell <frank@gocirro.com>
2013-08-12 00:24:13 -0600

    styled login page

--- sites/all/themes/clim/templates/page--user--%.tpl.php
+++ sites/all/themes/clim/templates/page--user--%.tpl.php
@@ -6,11 +6,12 @@
       <div id="content-area">
         <?php //print render($page['content']) ?>
       </div>
-      <h5>Welcome Viviane</h5>
+      <h5>Accueil Viviane</h5>
 
       <ul>
       	<li><a href="/admin/content">Website Content</a></li>
-      	<li><a href="/node/13/edit?destination=admin/content">Edit Artist Statement</a></li>
+      	<li><a href="/node/13/edit?destination=admin/content">Edit About Info</a></li>
+        <li><a href="/node/add/work">Add New Work</a></li>
   			<li><a href="/">Return to Homepage</a></li>
       	<li><a href="/user/logout">Logout</a></li>
       </ul>
--- sites/all/themes/clim/templates/page--user.tpl.php
+++ sites/all/themes/clim/templates/page--user.tpl.php
@@ -1,19 +1,14 @@
 <div class="container <?php print $classes; ?>"<?php print $attributes; ?> >
-	 
-	 <div class="row">
-    <div class="span6"> 
-    	<div id="logo">
-	       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-			      <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
-			    </a>
-	    </div>
-  	</div>
+	<div class="login-wrap">
+  	<div id="login-logo">
+       <a href="/" title="<?php print t('Home'); ?>" rel="home" id="logo">
+		      <img src="/sites/all/themes/clim/images/viviane_loading.gif"/>
+		    </a>
+    </div>
 
-    <div class="span6">
-      <?php print $messages; ?>
-      <?php print render($page['content']) ?>
-    
-    	<span id="forgot"><a href="user/password">forgot your password?</a></span>
-   	<div class="span6">
+    <?php print $messages; ?>
+    <?php print render($page['content']) ?>
+  
+  	<span id="forgot"><a href="user/password">forgot your password?</a></span>
   </div>
 </div> <!-- /page -->
